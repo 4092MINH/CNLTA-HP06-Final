e291150f787752d936ded437d92139e4746b9a53
Phan Nguyen Khanh Minh <phannguyenkhanhminh@gmail.com>
2025-06-22 10:21:35 +0700

    Initial Commit

---
+++ CNLTA-HP06-Final/CNLTA_HP06_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  CNLTA_HP06_FinalApp.swift
+//  CNLTA-HP06-Final
+//
+//  Created by Phan Nguyễn Khánh Minh on 22/6/25.
+//
+
+import SwiftUI
+
+@main
+struct CNLTA_HP06_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CNLTA-HP06-Final/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CNLTA-HP06-Final
+//
+//  Created by Phan Nguyễn Khánh Minh on 22/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
